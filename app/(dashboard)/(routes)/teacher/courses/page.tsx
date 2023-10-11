af360131eb7c8434699ef3b33315eb8d6a4dcd5c
mstout60 <mstout@eprospector.com>
2023-10-10 20:42:13 -0500

    Initial

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const CoursesPage = () => {
+    return (
+        <Link href="/teacher/create">
+            <div className="p-6">
+                <Button>
+                    New Course
+                </Button>
+            </div>
+        </Link>
+    );
+}
+
+export default CoursesPage;
