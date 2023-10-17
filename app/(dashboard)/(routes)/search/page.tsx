544053bdcf17d6a99cf530ef4611cdd476bd2f94
mstout60 <mstout@eprospector.com>
2023-10-17 13:37:46 -0500

    Add course list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,27 +1,50 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 import { db } from "@/lib/db";
+import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
 
 import { Categories } from "./_components/categories";
-import { SearchInput } from "@/components/search-input";
 
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+};
 
-const SearchPage = async () => {
+const SearchPage = async ({
+    searchParams
+}: SearchPageProps) => {
+    const { userId } = auth();
+
+    if (!(userId)) {
+        return redirect("/");
+    }
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc"
         }
     });
 
+    const courses = await getCourses({
+        userId,
+        ...searchParams,
+    });
 
     return (
         <>
-        <div className="px-6 pt-6 md:hiddend md:mb-0 block">
-            <SearchInput />
+            <div className="px-6 pt-6 md:hiddend md:mb-0 block">
+                <SearchInput />
 
-        </div>
+            </div>
             <div className="p-6">
                 <Categories
                     items={categories}
                 />
+                <CoursesList items={courses} />
             </div>
         </>
     );
