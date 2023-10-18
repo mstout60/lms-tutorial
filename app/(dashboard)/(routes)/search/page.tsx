db3c2f18ba747a13e83a138b9419c45dd2d334dc
mstout60 <mstout@eprospector.com>
2023-10-18 08:43:25 -0500

    Added new chapter layout

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -38,9 +38,8 @@ const SearchPage = async ({
         <>
             <div className="px-6 pt-6 md:hiddend md:mb-0 block">
                 <SearchInput />
-
             </div>
-            <div className="p-6">
+            <div className="p-6 space-y-4">
                 <Categories
                     items={categories}
                 />
